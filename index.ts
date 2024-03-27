4e0e1dd771aa5c7b6ef12308728adaf25b7c61f6
unknown <murkdr64@gmail.com>
2024-03-27 00:45:34 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+        message :"Enter First Number", type :"number", name :"firstNumber"},
+        {
+       message : "Enter Second Number", type : "number", name :"secondNumber"},
+    {
+        message : "Select one of the operator to perform Task",
+        type :"list",
+        name : "operator",
+        choices :["Addition", "Subtraction","Multiplication", "Division"]
+    },    
+    
+]);
+if(answer.operator ==="Addition"){
+console.log(answer.firstNumber+answer.secondNumber)
+}
+else if(answer.operator ==="Subtraction"){
+    console.log(answer.firstNumber-answer.secondNumber)
+}
+else if(answer.operator ==="multiplication"){
+ console.log(answer.firstNumber*answer.secondNumber)
+}
+else if(answer.operator==="Division"){
+    console.log(answer.firstNumber/answer.secondNumber)
+}
+else{
+    console.log("First enter Number");
+}
+
+
+
+
